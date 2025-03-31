87ad800c3be92de7e260d21b753f056b894af282
deidave <atudav15@gmail.com>
2025-03-31 11:50:04 +0300

    Completed hard-coded UI
    Added analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const Analytics = () => {
-  return <div className="analytics">Analytics</div>;
+  return (
+    <div className="analytics">
+      <div className="completed">
+        <h3>completed tasks</h3>
+        <h1>04</h1>
+      </div>
+      <div className="pending">
+        <h3>pending tasks</h3>
+        <h1>15</h1>
+      </div>
+      <div className="created-active">
+        <div className="created">
+          <h3>Tasks created</h3>
+          <h1>1,500</h1>
+        </div>
+        <div className="active">
+          <h3>
+            <span>25k+</span> active users
+          </h3>
+          <div>
+            <img src="/assets/active_1.jpeg" alt="ap_1" />
+            <img src="/assets/active_2.jpeg" alt="ap_2" />
+            <img src="/assets/active_3.jpeg" alt="ap_3" />
+            <img src="/assets/active_4.jpeg" alt="ap_4" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Analytics;
